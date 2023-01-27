26cc8a29decb8a633f83060874e3146fb670cf80
Michael Boyd <9999.michael@gmail.com>
2023-01-27 00:30:45 -0600

    Updated int and decmials tests and implementation.

--- src/examples/01_data_types/01_int/main.cpp
+++ src/examples/01_data_types/01_int/main.cpp
@@ -1,5 +1,11 @@
+#include "int.h"
+#include <iostream>
+
+using std::cout;
+
 int main()
 {
-
+    int num = 5;
+    cout << "Echo the input integer [" << num << "]: " << echo_variable(num) << "\n";
     return 0;
 }
--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,21 +1,64 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "int.h"
+#include "decimals.h"
+#include "char.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
+// TEST_CASE("Verify Test Configuration", "verification") {
+// 	REQUIRE(true == true);
+// }
 
 /*test case echo_variable */
+TEST_CASE("Test echo_variable", "input number should equal output number")
+{
+	REQUIRE(echo_variable(5) == 5);		// assertion
+	REQUIRE(echo_variable(10) == 10);	// assertion
+	REQUIRE(echo_variable(100) == 100); // assertion
+}
 
 /*test case add_to_double_1 with 0 as parameter*/
+TEST_CASE("Verify add_to_double_1 with 0 as parameter ", "verify number returned is .9")
+{
+	double input = 0;
+	double expected = 0.9;
+	REQUIRE((add_to_double_1(input) * 10) == (expected * 10));
+}
 
 /*test case add_to_double_1 with 1 as parameter*/
+TEST_CASE("Verify add_to_double_1 with 1 as parameter ", "verify number returned is 1.9")
+{
+	double input = 1;
+	double expected = 1.9;
+	REQUIRE((add_to_double_1(input) * 10) == (expected * 10));
+}
 
 /*test case add_to_double_2 with 0 as parameter*/
+TEST_CASE("Verify add_to_double_2 with 0 as parameter ", "verify number returned is 1.5")
+{
+	double input = 0;
+	double expected = 1.5;
+	REQUIRE((add_to_double_2(input) * 10) == (expected * 10));
+}
 
 /*test case add_to_double_2 with 1 as parameter*/
+TEST_CASE("Verify add_to_double_2 with 1 as parameter ", "verify number returned is 2.5")
+{
+	double input = 1;
+	double expected = 2.5;
+	REQUIRE((add_to_double_2(input) * 10) == (expected * 10));
+}
 
 /*test case char test ASCII values */
+TEST_CASE("Verify Test characters as numbers", "verify char input equals a int value")
+{
+	REQUIRE(get_char_ascii_value('A') == 65);
+	REQUIRE(get_char_ascii_value('a') == 97);
+}
 
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
-
+TEST_CASE("Verify Test characters as numbers for Joe", "verify char input equals a int value for Joe")
+{
+	REQUIRE(get_char_ascii_value('J') == 74);
+	REQUIRE(get_char_ascii_value('o') == 111);
+	REQUIRE(get_char_ascii_value('e') == 101);
+}
