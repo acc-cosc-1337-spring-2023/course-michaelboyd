56011b4b6ae98024f7f36d5cae9bcce4c5f647c1
Michael Boyd <9999.michael@gmail.com>
2023-01-29 02:38:05 -0600

    Homework 1 - unit tests and implementation

--- src/homework/01_data_types/data_types.cpp
+++ src/homework/01_data_types/data_types.cpp
@@ -1,13 +1,14 @@
 #include "data_types.h"
 
-//example
+// example
 int add_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
 
-//write function code here
-
-
-
-
+// write function code here
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
+	return num1 * num2;
+}
--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,15 +1,26 @@
-﻿//write include statements
+﻿// write include statements
+#include "data_types.h"
+#include <iostream>
 
-
-//write namespace using statement for cout
-
+// write namespace using statement for cout
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num;
+	int num1;
+	int result;
 
-	return 0;
+	std::cout << "Enter a whole number: ";
+	std::cin >> num;
+	result = multiply_numbers(num);
+	std::cout << "The input [" << num << "] multiplied by 5 is: " << result << "\n";
+
+	num1 = 4;
+	result = multiply_numbers(num1);
+	std::cout << "The input [" << num1 << "] multiplied by 5 is: " << result << "\n";
+
+	sreturn 0;
 }
