321593e052a618b03edd034bbcd0b4955b572bf2
Michael Boyd <9999.michael@gmail.com>
2023-02-06 23:44:39 -0600

    Implement Restaurant Bill

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,41 @@
-#include "expressions.h"
+#include "hwexpressions.h"
+#include <iostream>
 
-//example
-int add_numbers(int num1, int num2)
+using std::cout;
+using std::string;
+
+/// @brief calculates sales tax for a given meal amount
+/// @param meal_amount
+/// @return sales tax amount
+double get_sales_tax_amount(double meal_amount)
 {
-	return num1 + num2;
+	return TAX_RATE * meal_amount;
 }
 
-//write function code here
+/// @brief calculates the tip amount given a meal amount and tip rate
+/// @param meal_amount
+/// @param tip_rate
+/// @return tip amount
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * (tip_rate / 100);
+}
 
+/// @brief displays a formatted receipt containg cost details of the meal
+/// @param meal_amount
+/// @param tax_amount
+/// @param tip_amount
+/// @param total
+void displayReceipt(double meal_amount, double tax_amount, double tip_amount, double total)
+{
+	string new_line_char = "\n";
 
-
-
+	cout << new_line_char;
+	cout << "=========================" << new_line_char;
+	cout << "Meal Amount: " << meal_amount << new_line_char;
+	cout << "Sales Tax  : " << tax_amount << new_line_char;
+	cout << "Tip Amount : " << tip_amount << new_line_char;
+	cout << "Total      : " << total << new_line_char;
+	cout << "=========================" << new_line_char;
+	cout << new_line_char;
+}
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -1,14 +1,15 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "hwexpressions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
-}
-
-TEST_CASE("Verify sum_numbers function") 
+TEST_CASE("Test Get Sales Tax", "verify function that cacluates sales tax")
 {
-	REQUIRE(add_numbers(5, 5) == 10);
-	REQUIRE(add_numbers(10, 10) == 20);
+	REQUIRE(get_sales_tax_amount(10) == 0.675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
 }
 
+TEST_CASE("Test Get Tip Amount", "verify function that calculates tip amount")
+{
+	REQUIRE(get_tip_amount(20, 15) == 3);
+	REQUIRE(get_tip_amount(20, 20) == 4);
+}
