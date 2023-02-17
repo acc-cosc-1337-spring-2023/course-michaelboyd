28cdd195bb4ca99c49743130613fa6dc75096588
Michael Boyd <9999.michael@gmail.com>
2023-02-16 23:22:46 -0600

    Add prototypes, implementation, and unit tests.

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,69 @@
-//write include statement for decisions header
+#include "decisions.h"
+#include <limits>
 
+/// @brief Convert a number grade to a letter grade
+/// @param grade numeric value for the grade to convert
+/// @return alpha representation of the grade
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string result;
 
-//Write code for function(s) code here
+    if (grade >= 90)
+    {
+        result = "A";
+    }
+    else if (grade < 90 && grade >= 80)
+    {
+        result = "B";
+    }
+    else if (grade < 80 && grade >= 70)
+    {
+        result = "C";
+    }
+    else if (grade < 70 && grade >= 60)
+    {
+        result = "D";
+    }
+    else if (grade < 60 && grade >= 0)
+    {
+        result = "F";
+    }
+    else
+    {
+        result = "Invalid Input";
+    }
+    return result;
+}
+
+/// @brief Convert a number grade to a letter grade
+/// @param grade numeric value for the grade to convert
+/// @return alpha representation of the grade
+std::string get_letter_grade_using_switch(int grade)
+{
+    std::string result;
+    const int max_value = std::numeric_limits<int>::max();
+
+    switch (grade)
+    {
+    case 90 ... max_value:
+        result = "A";
+        break;
+    case 80 ... 89:
+        result = "B";
+        break;
+    case 70 ... 79:
+        result = "C";
+        break;
+    case 60 ... 69:
+        result = "D";
+        break;
+    case 0 ... 59:
+        result = "F";
+        break;
+    default:
+        result = "Invalid Input";
+        break;
+    }
+
+    return result;
+}
