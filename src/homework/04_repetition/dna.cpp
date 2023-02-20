004eafab1661bb0d4c5af9169a0e6d73ecce0485
Michael Boyd <9999.michael@gmail.com>
2023-02-20 01:55:12 -0600

    Implement factorial method

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,24 @@
-//add include statements
+#include "dna.h"
 
-//add function(s) code here
+int factorial(int num)
+{
+    int factorial = num;
+
+    if (num == 0 || num == 1)
+    {
+        return 1;
+    }
+
+    while (num > 1)
+    {
+        factorial = factorial * (num - 1);
+        num--;
+    }
+    
+    return factorial;
+}
+
+int gcd(int num1, int num2)
+{
+    return 0;
+}
--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,5 @@
-//add include statements
 
-/*
-Write prototype for function(s)
-*/
+
+int factorial(int num);
+
+int gcd(int num1, int num2);
