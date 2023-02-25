d20b88bc7a0bbf2c0c38f411243f3ee254e271dd
Michael Boyd <9999.michael@gmail.com>
2023-02-24 21:57:54 -0600

    In class examples

---
+++ src/examples/06_strings/02_functions/function_str.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void str_val_ref_param(std::string &str1, std::string &str2, const std::string &str3);
--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,17 @@
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+
 int main()
 {
+    vector <int> nums;
+    cout<<nums.size()<<"\n";
+    cout<<nums.capacity()<<"\n\n";
+
+    
+
+
     return 0;
 }
