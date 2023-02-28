c7d8cdde98037a9ff74f874a1213dcba362a9d5c
Michael Boyd <9999.michael@gmail.com>
2023-02-27 22:04:58 -0600

    add functions and test cases

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,54 @@
-//add include statements
+#include "func.h"
+#include <iostream>
 
-//add function code here
+using std::cout;
+using std::string;
+
+double get_gc_content(const string &dna)
+{
+    double gc_content_count = 0;
+    for (auto &c : dna)
+    {
+        if (c == 'G' || c == 'C')
+        {
+            gc_content_count++;
+        }
+    }
+    return gc_content_count / dna.length();
+}
+
+string get_reverse_string(string dna)
+{
+    string result;
+    for (auto i = dna.length(); i > 0; i--)
+    {
+        result.push_back(dna[i - 1]);
+    }
+    return result;
+}
+
+string get_dna_compliment(string dna)
+{
+    string dna_compliment = get_reverse_string(dna);
+    for (auto i = 0; i < dna_compliment.length(); i++)
+    {
+        auto c = dna_compliment[i];
+        if (c == 'A')
+        {
+            dna_compliment[i] = 'T';
+        }
+        else if (c == 'T')
+        {
+            dna_compliment[i] = 'A';
+        }
+        else if (c == 'G')
+        {
+            dna_compliment[i] = 'C';
+        }
+        else if (c == 'C')
+        {
+            dna_compliment[i] = 'G';
+        }
+    }
+    return dna_compliment;
+}
--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,20 @@
-//add function(s) prototype here
+#include <string>
+
+/// @brief The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
+/// @param dna 
+/// @return the percentage of symbols in the string that are 'C' or 'G' 
+double get_gc_content(const std::string &dna);
+
+
+/// @brief In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'. The reverse complement of a DNA string is the string, then taking the complement of each symbol.
+/// @param dna 
+/// @return The reverse complement sc of s.
+std::string get_dna_compliment(std::string dna);
+
+
+/// @brief Reverses the order of characters of the input string
+/// @param dna 
+/// @return the input string in reverse
+std::string get_reverse_string(std::string dna);
+
+
